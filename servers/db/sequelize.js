d13bb991cd462b0b116e1b6f100b591d06e9cdb5
尹孟佳 <3495314473@qq.com>
2024-05-30 13:34:27 +0800

    fix:数据库bug

--- servers/db/sequelize.js
+++ servers/db/sequelize.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize'
 import mysql2 from 'mysql2'
-const sequelize = new Sequelize(process.env.NEXT_PUBLIC_DATABASE,process.env.NEXT_PUBLIC_USERNAME,process.env.NEXT_PUBLIC_PASSWORD,{
-  host: process.env.NEXT_PUBLIC_HOST,
+const sequelize = new Sequelize(process.env.NEXT_PUBLIC_MYSQL_DATABASE,process.env.NEXT_PUBLIC_MYSQL_USERNAME,process.env.NEXT_PUBLIC_MYSQL_PASSWORD,{
+  host: process.env.NEXT_PUBLIC_MYSQL_HOST,
   port:3306,
   dialect: "mysql",
   dialectModule:mysql2
--- services/config/index.ts
+++ services/config/index.ts
@@ -1,4 +1,4 @@
 export const TIME_OUT = 30000;
 
-const BASE_URL = "http://localhost:3000/apis";
+const BASE_URL = `http://${process.env._MYSQL_HOST}/apis`;
 export { BASE_URL };
